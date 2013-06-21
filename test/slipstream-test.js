be2280d7370335c8a639bff568d8928cf4e134ae
Sean 'Blitz' Homer <shomer@rcdesign.com>
2013-06-21 09:08:12 -0400

    - Revisions to test case to reflect class changes - Basic error checking on object creation to ensure essential parameters are always present, else throw critical error

--- test/slipstream-test.js
+++ test/slipstream-test.js
@@ -7,29 +7,32 @@ Tinytest.add('MeteorFile - read', function (test) {
 });
 
 Tinytest.add('Slipstream.Drift - create object', function (test) {
-	var Project = Slipstream.Drift('project', 'slug', {
-		title  : Slipstream.Column({
-			name        : 'title',
-			label       : 'Project Title',
-			type        : InputTypes.text,
-			placeholder : 'Title',
-			default     : ''
-		}),
-		desc   : Slipstream.Column({
-			name        : 'desc',
-			label       : 'Project Description',
-			type        : InputTypes.textarea,
-			placeholder : 'Description',
-			default     : ''
-		}),
-		author : Slipstream.Column({
-			name        : 'author',
-			label       : 'Author',
-			type        : InputTypes.hidden,
-			placeholder : 'Author',
-			default     : ''
-		})
-	});
+	var Project = Slipstream.Drift({
+		name           : 'project',
+		referenceField : 'slug',
+		columns        : {
+			title  : Slipstream.Column({
+				name        : 'title',
+				label       : 'Project Title',
+				type        : InputTypes.text,
+				placeholder : 'Title',
+				default     : ''
+			}),
+			desc   : Slipstream.Column({
+				name        : 'desc',
+				label       : 'Project Description',
+				type        : InputTypes.textarea,
+				placeholder : 'Description',
+				default     : ''
+			}),
+			author : Slipstream.Column({
+				name        : 'author',
+				label       : 'Author',
+				type        : InputTypes.hidden,
+				placeholder : 'Author',
+				default     : ''
+			})
+		}});
 	var o = {};
 	test.equal(Project, o, 'Likely to be different');
 });
